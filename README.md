edb8bd574657e359750990b37a2d4a730a89e32a
Robert Cope <rpcope1@gmail.com>
2016-01-29 16:45:08 -0700

    Restructuring...

---
+++ README.md
@@ -0,0 +1,2 @@
+# Python Test Toolbox
+### A simple set of enhancements and tools for unittest compatible tests.
--- testtoolbox/testflow.py
+++ testtoolbox/testflow.py
@@ -1,74 +1,9 @@
-from __future__ import print_function
-from functools import wraps, partial
-from collections import namedtuple
-import traceback
 import inspect
 import textwrap
-import time
-import ctypes
+import traceback
+from functools import wraps, partial
 
-__author__ = 'Robert Cope'
-
-
-class ANSITermCodes(object):
-    WHITE = '\033[37m'
-    CYAN = '\033[36m'
-    PURPLE = '\033[35m'
-    BLUE = '\033[34m'
-    YELLOW = '\033[33m'
-    GREEN = '\033[32m'
-    RED = '\033[31m'
-    BLACK = '\033[30m'
-    NORMAL_COLOR = '\033[39m'
-    RESET = '\033[0m'
-    BOLD = '\033[1m'
-    HALF_BRIGHT = '\033[1m'
-    NORMAL_BRIGHT = '\033[22m'
-    UNDERLINE = '\033[4m'
-    UNDERLINE_OFF = '\033[24m'
-    BLINK = '\033[5m'
-    BLINK_OFF = '\033[25m'
-
-
-def _output_formatter(output_code, text, terminator_code=ANSITermCodes.RESET):
-    return "%s%s%s" % (output_code, str(text), terminator_code)
-
-
-purple = partial(_output_formatter, ANSITermCodes.PURPLE, terminator_code=ANSITermCodes.NORMAL_COLOR)
-green = partial(_output_formatter, ANSITermCodes.GREEN, terminator_code=ANSITermCodes.NORMAL_COLOR)
-red = partial(_output_formatter, ANSITermCodes.RED, terminator_code=ANSITermCodes.NORMAL_COLOR)
-yellow = partial(_output_formatter, ANSITermCodes.YELLOW, terminator_code=ANSITermCodes.NORMAL_COLOR)
-blue = partial(_output_formatter, ANSITermCodes.BLUE, terminator_code=ANSITermCodes.NORMAL_COLOR)
-cyan = partial(_output_formatter, ANSITermCodes.CYAN, terminator_code=ANSITermCodes.NORMAL_COLOR)
-black = partial(_output_formatter, ANSITermCodes.BLACK, terminator_code=ANSITermCodes.NORMAL_COLOR)
-white = partial(_output_formatter, ANSITermCodes.WHITE, terminator_code=ANSITermCodes.NORMAL_COLOR)
-bold = partial(_output_formatter, ANSITermCodes.BOLD, terminator_code=ANSITermCodes.NORMAL_BRIGHT)
-half_bright = partial(_output_formatter, ANSITermCodes.HALF_BRIGHT, terminator_code=ANSITermCodes.NORMAL_BRIGHT)
-underline = partial(_output_formatter, ANSITermCodes.UNDERLINE, terminator_code=ANSITermCodes.UNDERLINE_OFF)
-blinking = partial(_output_formatter, ANSITermCodes.BLINK, terminator_code=ANSITermCodes.BLINK_OFF)
-
-
-def _print_formatter(color_str, *args):
-    args = args + (ANSITermCodes.RESET,)
-    print(color_str, *args)
-
-
-print_purple = partial(_print_formatter, ANSITermCodes.PURPLE)
-print_green = partial(_print_formatter, ANSITermCodes.GREEN)
-print_red = partial(_print_formatter, ANSITermCodes.RED)
-print_yellow = partial(_print_formatter, ANSITermCodes.YELLOW)
-print_cyan = partial(_print_formatter, ANSITermCodes.CYAN)
-print_blue = partial(_print_formatter, ANSITermCodes.BLUE)
-print_black = partial(_print_formatter, ANSITermCodes.BLACK)
-print_white = partial(_print_formatter, ANSITermCodes.WHITE)
-print_bold = partial(_print_formatter, ANSITermCodes.BOLD)
-print_half_bright = partial(_print_formatter, ANSITermCodes.HALF_BRIGHT)
-print_underline = partial(_print_formatter, ANSITermCodes.UNDERLINE)
-print_blinking = partial(_print_formatter, ANSITermCodes.BLINK)
-
-
-TestOutputFunctions = namedtuple("TestOutputFunctions", ["info", "pass_", "ignore", "warn", "fail"])
-DefaultOutputFunctions = TestOutputFunctions(print_purple, print_green, print_green, print_yellow, print_red)
+from testtoolbox.output import DefaultOutputFunctions
 
 
 def should(behavior):
@@ -106,7 +41,7 @@ def _test_method_decorator_constructor(prefix, subject, predicate, width=80, out
                 output_msg = "Result: [NOT IMPLEMENTED]"
                 output_functions.warn(wrap_text_cleanly(output_msg, width=width))
             except IgnoreTest as e:
-                ignore_msg = "(%s)" % e.message if e.message else ""
+                ignore_msg = "(%s)" % getattr(e, 'message') if getattr(e, 'message') else ""
                 output_msg = "Result: [IGNORED] %s" % ignore_msg
                 output_functions.warn(wrap_text_cleanly(output_msg, width=width))
             except BaseException as e:
@@ -116,6 +51,7 @@ def _test_method_decorator_constructor(prefix, subject, predicate, width=80, out
                 raise
         return decorated
     return decorator
+
 
 case_descriptor = partial(_test_method_decorator_constructor, "")
 unit_descriptor = partial(_test_method_decorator_constructor, "")
@@ -152,6 +88,7 @@ def _test_case_class_decorator_constructor(prefix, name, print_class_docstring=T
         return WrappedClass
 
     return decorator
+
 
 case_name = partial(_test_case_class_decorator_constructor, "")
 feature = partial(_test_case_class_decorator_constructor, "Feature: ")
@@ -260,54 +197,3 @@ class BDD(object):
                 entry, exception_data = map(wrap_text, strs)
                 formatted_str = "%s\n\t\t%s" % (entry, exception_data)
                 self.output_functions.fail(formatted_str)
-
-
-def modify_buffer_object(source_buffer, dest_buffer, nbytes=None):
-    """
-    Modify a python object that supports the buffer interface internally.
-    This is useful for testing things like socket.recv_into()
-
-    dest_buffer must be smaller than source_buffer, or the code will assert.
-
-    :param source_buffer: The source for the new data to be written into dest_buffer.
-    :param dest_buffer: The buffer to be modified inline.
-    :param nbytes: (OPTIONAL) The maximum number of bytes to write into the dest_buffer.
-        If set, the number of bytes written is the minimum of nbytes and the source length.
-        Default: None
-    :return: The total number of bytes written into dest_buffer.
-    """
-    assert len(source_buffer) < len(dest_buffer)
-    copy_len = len(source_buffer) if nbytes is None else min(nbytes, source_buffer)
-    buffer_ptr = (ctypes.c_byte * len(dest_buffer)).from_buffer(dest_buffer)
-    new_value_ptr = (ctypes.c_byte * len(source_buffer)).from_buffer_copy(source_buffer)
-    buffer_ptr[:copy_len] = new_value_ptr[:]
-    return copy_len
-
-
-def await_condition(description, condition_eval_callable, on_failure=lambda: True, timeout=10, poll_ms=100):
-    """
-    Await a condition function to return True, otherwise assert.
-
-    :param description: A string description of the condition we are awaiting.
-    :param condition_eval_callable: The callable of arity 0 (function, lambda, etc.) to monitor.
-        This should return False when not completed, and return True when done.
-    :param on_failure: (OPTIONAL) A callable of arity 0 that is called when a failure condition occurs.
-        Default: NOOP
-    :param timeout: (OPTIONAL) The number of seconds to wait for the condition to become True.
-        Default: 10 s
-    :param poll_ms: (OPTIONAL) The period of time between checking the condition.
-        Default: 100 ms
-    :return: None. Will raise an AssertionError if the condition did not return True in the allotted time.
-    """
-    poll_s = poll_ms / 1000.0
-    start_time = time.time()
-
-    def should_continue():
-        return time.time() - start_time < timeout
-
-    while not condition_eval_callable():
-        if not should_continue():
-            on_failure()
-            raise AssertionError("Awaiting condition %s has timed out after %f seconds"
-                                 "" % (str(description), float(timeout)))
-        time.sleep(poll_s)
